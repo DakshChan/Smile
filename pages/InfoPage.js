66ef6fe9e0b9fb74b3ecd7b640d4947eb1355dcd
Hammad Sheikh <hammad.shaeque@gmail.com>
2022-05-01 00:50:17 -0400

    Added infoPage

---
+++ pages/InfoPage.js
@@ -0,0 +1,41 @@
+import {Box, Center, Heading, Text} from "@chakra-ui/react";
+
+
+const Card = () =>
+    {
+        return(
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow={"2xl"} width={736} height={895}>
+        <Box m="5">
+            <Heading m="5" mb="0" as="h4" size="md" mt="10">
+                How do you use Smile?
+            </Heading>
+            <Text m="5" mt="15">
+                <Text>
+                    There are three major steps for using smile!
+                    First component consists of smiling for at least 10 seconds.
+                    Studies show that <a href={'https://www.tandfonline.com/doi/full/10.1080/17437199.2022.2052740'}>even forced smiling for
+                    a short period of time can help reduce stress, benefiting both your physical and mental health! </a>
+                    The second component consists of positive affirmations.
+                    Arquiza asserts that
+                    <a href={'https://scholar.dominican.edu/scw/SCW2020/conference-presentations/63/'}> positive
+                        affirmations can improve one's self esteem and overall happiness. </a>
+                    Thus, given three affirmations, we use sentimental analysis to determine whether the affirmations
+                    are positive or not!
+                    The final part of our mile is giving supplementary resources to support mental health such as music, quotes,
+                    and other resources.
+                </Text>
+            </Text>
+        </Box>
+    </Box>)
+};
+
+
+export default function InfoPage() {
+    return (
+        <Center>
+        <Card/>
+    </Center>
+)
+}
+
+
