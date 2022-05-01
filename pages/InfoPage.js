06b4de94bc0c555a552205eae6eba2f120c6852a
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-05-01 01:08:17 -0400

    add info changes

--- pages/InfoPage.js
+++ pages/InfoPage.js
@@ -10,20 +10,22 @@ const Card = () =>
                 How do you use Smile?
             </Heading>
             <Text m="5" mt="15">
-                <Text>
                     There are three major steps for using smile!
                     First component consists of smiling for at least 10 seconds.
                     Studies show that <a href={'https://www.tandfonline.com/doi/full/10.1080/17437199.2022.2052740'}>even forced smiling for
-                    a short period of time can help reduce stress, benefiting both your physical and mental health! </a>
+                    a short period of time can help reduce stress, benefiting both your physical and mental health! </a> <br/>
+            </Text>
+            <Text m="5" mt="15">
                     The second component consists of positive affirmations.
                     Arquiza asserts that
                     <a href={'https://scholar.dominican.edu/scw/SCW2020/conference-presentations/63/'}> positive
                         affirmations can improve one's self esteem and overall happiness. </a>
                     Thus, given three affirmations, we use sentimental analysis to determine whether the affirmations
                     are positive or not!
+                </Text>
+                <Text m="5" mt="15">
                     The final part of our mile is giving supplementary resources to support mental health such as music, quotes,
                     and other resources.
-                </Text>
             </Text>
         </Box>
     </Box>)
