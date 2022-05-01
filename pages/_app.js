eb14fd7947e2d0cfa89c1b30a8e015d6c7b4753d
Hammad Sheikh <hammad.shaeque@gmail.com>
2022-04-30 23:46:47 -0400

    Added logo on top left

--- pages/_app.js
+++ pages/_app.js
@@ -16,8 +16,8 @@ function LandingPage({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
 
-            <Flex alignItems='center' gap={'2'}>
-                <Button pl={31}>Logo Here</Button>
+            <Flex alignItems='center' gap={'2'} pl={18}>
+                <Image src={'/Logo.png'} width={132} height={52}/>
                 <Spacer/>
                 <ButtonGroup gap={13.86} pr={61.86} pt={35}>
                     <InfoIcon onClick={handleInfoOnclick} w={30} h={30}/>
